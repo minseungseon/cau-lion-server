dad9da3808bb6ad56ca74e437c7dd55c5521964b
oereo <dlstpgns0406@gmail.com>
2020-04-24 03:12:39 +0900

    [FIX] urls.py

--- backend_django/login_api/urls.py
+++ backend_django/login_api/urls.py
@@ -3,7 +3,7 @@ from .views import TestAPI, RegistrationAPI, LoginAPI, UserAPI, ProfileUpdateAPI
 
 
 urlpatterns = [
-    path('test/', TestAPI),
+    path('', TestAPI),
     path("auth/register/", RegistrationAPI.as_view()),
     path("auth/login/", LoginAPI.as_view()),
     path("auth/user/", UserAPI.as_view()),
