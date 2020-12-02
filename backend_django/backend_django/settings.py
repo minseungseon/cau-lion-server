5a1bd89de55246cafc8740cdaba416de0b08e7e4
oereo <dlstpgns0406@gmail.com>
2020-12-02 11:38:07 +0900

    [FIX] settings

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -1,43 +1,41 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '9mb)q$eamb4on4k3*7y7io%nb(5sm9!+vhjurq_y=4k(+bd)pt'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','caulion-env.eba-vw5ggty2.us-west-2.elasticbeanstalk.com']
-#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'caulion-env.eba-vw5ggty2.us-west-2.elasticbeanstalk.com']
 
-#REST_FRAMEWORK 선민승 04-20 추가
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES" : ("knox.auth.TokenAuthentication",),
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'#이거 swagger써줄때 꼭 써야되요!
+    "DEFAULT_AUTHENTICATION_CLASSES": ("knox.auth.TokenAuthentication",),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'  # 이거 swagger써줄때 꼭 써야되요!
 }
 
-# Application definition
-
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+]
+
+PROJECT_APPS = [
     'login_api',
+]
+
+COMMON_APPS = [
     'rest_framework',
     'knox',
     'corsheaders',
     'rest_auth',
-    'rest_framework_swagger', # api 자동문서화 swagger!
+    'rest_framework_swagger',  # api 자동문서화 swagger!
 ]
+
+INSTALLED_APPS = DEFAULT_APPS + PROJECT_APPS + COMMON_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,11 +45,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True # 개발 중에만 True를 쓰고, 실제 배포시 False로 바꾼 후, 아래 whitelist를 쓰면 됩니다.
+CORS_ORIGIN_ALLOW_ALL = True  # 개발 중에만 True를 쓰고, 실제 배포시 False로 바꾼 후, 아래 whitelist를 쓰면 됩니다.
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:3000',
@@ -59,7 +57,7 @@ CORS_ALLOW_CREDENTIALS = True
 #     'http://localhost:3000',
 # ]
 
-#] If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+# ] If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
 
 ROOT_URLCONF = 'backend_django.urls'
 
@@ -82,9 +80,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend_django.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -92,9 +87,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -111,12 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-#TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
@@ -125,9 +114,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# static
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
