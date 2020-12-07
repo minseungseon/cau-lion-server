27dca4189a5c155297d1c2b8ef023b02a6ee234a
oereo <dlstpgns0406@gmail.com>
2020-12-07 11:12:27 +0900

    Fix the format

--- backend_django/login_api/views.py
+++ backend_django/login_api/views.py
@@ -1,18 +1,19 @@
-#2020-04-20 minseung seon modified.
-#generic 기반 클래스 뷰: 기본적인 기능을 모두 포함하는 뷰 
+# 2020-04-20 minseung seon modified.
+# generic 기반 클래스 뷰: 기본적인 기능을 모두 포함하는 뷰
 
-from rest_framework import viewsets, permissions, generics, status
-from rest_framework.response import Response
-from rest_framework.views import APIView 
-from rest_framework.decorators import api_view
 from knox.models import AuthToken
-from .serializers import CreateUserSerializer, UserSerializer, LoginUserSerializer, ProfileSerializer
+from rest_framework import permissions, generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Profile
+from .serializers import CreateUserSerializer, UserSerializer, LoginUserSerializer, ProfileSerializer
 
-@api_view(['GET']) #해당 함수 view에서 처리할 http 메소드 
 
-def TestAPI(request):   #GET 127.0.0.1:8000/login_api/test/ 로 요청을 보내어 테스트용으로 This is API Test를 출력하는 api 를 만듦 
+@api_view(['GET'])  # 해당 함수 view에서 처리할 http 메소드
+def TestAPI(request):  # GET 127.0.0.1:8000/login_api/test/ 로 요청을 보내어 테스트용으로 This is API Test를 출력하는 api 를 만듦
     return Response("This is API Test")
+
 
 class RegistrationAPI(generics.GenericAPIView):
     serializer_class = CreateUserSerializer
@@ -33,21 +34,23 @@ class RegistrationAPI(generics.GenericAPIView):
             }
         )
 
+
 class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginUserSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception= True)
+        serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
         return Response(
             {
                 "user": UserSerializer(
                     user, context=self.get_serializer_context()
                 ).data,
-                "token": AuthToken.objects.create(user)[1], #1의 의미는 무엇일까..
+                "token": AuthToken.objects.create(user)[1],  # 1의 의미는 무엇일까..
             }
         )
+
 
 class UserAPI(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -55,6 +58,7 @@ class UserAPI(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
 
 class ProfileUpdateAPI(generics.UpdateAPIView):
     lookup_field = "user_pk"
